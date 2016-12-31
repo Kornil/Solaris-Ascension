228923ee0477882847809f76002d709b03514b9c
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-12-31 12:16:03 +0100

    new spaceships(cruiser, frigate, corvette)
    cruiser movement, shoot, health, textures
    frigate movement, shoot, health, textures
    corvette movement, textures(doesnt shoot or have hp)

--- script.js
+++ script.js
@@ -7,40 +7,60 @@ const Height = 500, Width = 950,
     playerIdle2: document.getElementById("playership2"),
     playerTurbo: document.getElementById("playershipTurbo"),
     playerSlow: document.getElementById("playershipSlow"),
-    enemyProj: document.getElementById("enemyProj"),
+    frigateProj: document.getElementById("frigateProj"),
     playerProj: document.getElementById("playerProj"),
-    enemyCruiser: document.getElementById("enemyCruiser"),
-    cruiserProj: document.getElementById("cruiserProj2"),
+    cruiser: document.getElementById("cruiser"),
+    cruiserDamage: document.getElementById("cruiserDamage"),
+    cruiserProj: document.getElementById("cruiserProj"),
+    corvette: document.getElementById("corvette"),
+    frigate: document.getElementById("frigate"),
+    frigateDamage: document.getElementById("frigateDamage"),
   },
   shiptype = {
     cruiser: "cruiser",
-    frigate: "frigate"
+    frigate: "frigate",
+    corvette: "corvette",
   };
 
-let projectilesArray = [], gameOver = false, enemiesArray = [], lastSpawn = 0,
+let projectilesArray = [], gameOver = false, enemiesArray = [], lastSpawn = 0, // <-- time since last ship, global
   enemyNum = Math.floor((Math.random() * 25) + 75); // between 75-100 enemies, NOT USED
 
+// abstract class
 class allPrototypes {
   suicide(){
-    if(this.x + this.w < 0 || this.x > Width || this.y + this.h < 0 || this.y > Height)
+    if(this.x + this.w < 0 || this.x > Width || this.y + this.h < 0 || this.y > Height || (this.hp && this.hp === 0))
       return true;
     return false;
   }
   drawTexture(){
-    ctx.drawImage(this.texture, this.x, this.y, this.w, this.h);
+    let texture = this.texture;
+    if((this.hp+1 <= (this.maxHP)/2) && this.damageTexture) texture = this.damageTexture;
+    ctx.drawImage(texture, this.x, this.y, this.w, this.h);
   }
   move(){
-    //((this.enemy) ? this.x -= this.speedX : this.x += this.speedX);
     if(this.enemy){
-      if(this.type && this.type == shiptype.cruiser && this.goleft){
+      if(this.type === shiptype.cruiser && this.goleft){
+      // cruiser movement
         this.x -= this.speedX;
         if(this.x < Width*0.8)
           this.goleft = false;
-      }else if(this.type && this.type == shiptype.cruiser && !this.goleft){        
-        this.x += this.speedX;
-        if(this.x > Width-this.w)
-          this.goleft = true;
-      }else{   
+      }else if(this.type === shiptype.cruiser && !this.goleft){
+        if (this.originY >= this.y+this.verticalMov) this.down = true;
+        else if(this.originY <= this.y-this.verticalMov) this.down = false;
+        (this.down) ? this.y += this.speedY : this.y -= this.speedY;
+      }else if( this.type === shiptype.frigate){
+        // frigate movement
+        if(!this.evacuate && this.x < Width*0.7){
+          projectilesArray.push(new Projectile(this.x-10,this.y,this.h,true,this.type));
+          (this.y >= Height/2) ? this.direction = "up" : this.direction = "down" ;
+          this.evacuate = true;
+        }else if(this.evacuate){          
+          this.speedX += 0.1;
+          (this.direction === "up") ? this.y += this.speedX : this.y -= this.speedX;          
+        }
+        this.x -= this.speedX;
+      }else{
+        // enemy projectile movement
         this.x -= this.speedX;
         if(this.speedY)
           this.y -= this.speedY;
@@ -93,7 +113,7 @@ class Player extends allPrototypes {
     if(keys.isPressed(32)){
       let date = Date.now();
       if(date > this.shootDate + this.weaponDelay){
-        projectilesArray.push(new Projectile(this.x+this.w,this.y+this.h/2));
+        projectilesArray.push(new Projectile(this.x+this.w,this.y+this.h/2,false));
         this.shootDate = date;
       }
     }
@@ -104,26 +124,27 @@ let player = new Player();
 class Projectile extends allPrototypes {
   constructor(x,y,h,enemy,type){
     super();
-    this.h = 7;
     this.x = x-10;
     this.y = y+h/2;
     if(enemy){
       this.enemy = true;
-      if(type == shiptype.cruiser){
+      if(type === shiptype.cruiser){
         this.h = 16;
-        this.w = 20;
+        this.w = 20;        
         this.thrust = 4;
-        let tx = player.x - x, ty = player.y - y,
-        dist = Math.sqrt(tx*tx+ty*ty);
-        this.speedX = -(tx/dist)*this.thrust;
-        this.speedY = -(ty/dist)*this.thrust;
         this.texture = texture.cruiserProj;
-      }else{  
-        this.speedX = 7;
-        this.texture = texture.enemyProj;
+      }else if(type === shiptype.frigate){
+        this.h = 7;
         this.w = 14;
+        this.texture = texture.frigateProj;
+        this.thrust = 8;
       }
+      let tx = player.x - x, ty = player.y - y,
+      dist = Math.sqrt(tx*tx+ty*ty);
+      this.speedX = -(tx/dist)*this.thrust;
+      this.speedY = -(ty/dist)*this.thrust;
     }else{
+      this.h = 7;
       this.w = 7;
       this.speedX = 10;
       this.texture = texture.playerProj;
@@ -135,35 +156,48 @@ function enemyType(){
   let rand = Math.floor(Math.random() * 10) + 1,
     countCruisers = 0;
     enemiesArray.map( function(i){if(i.type == shiptype.cruiser) countCruisers++});
-  if(rand <= 2 && countCruisers < 3){
-    return shiptype.cruiser;
-  }
-  return shiptype.frigate;
+  if(rand === 1 && countCruisers < 3) return shiptype.cruiser;
+  else if(rand <= 5) return shiptype.frigate;
+  else return shiptype.corvette;
 }
 
 class Enemy extends allPrototypes {
   constructor(type){
     super();
-    if(type == shiptype.cruiser){
-      this.w = 56;
-      this.h = 56;
-      this.type = type;
+    this.type = type;
+    if(type === shiptype.cruiser){
+      this.w = 60;
+      this.h = 62;
       this.speedX = 0.5;
+      this.speedY = 0.5;
       this.fireRate = 3500;
+      this.maxHP = 5; // <- 5 shots to take down
+      this.hp = this.maxHP-1; // count from zero, used to change texture model on damage
       this.goleft = true;
-      this.texture = texture.enemyCruiser;
-    }else{
-      this.w = 28;
-      this.h = 28;
-      this.type = type;
-      this.speedX = 2;
-      this.fireRate = 1500;
-      this.texture = texture.enemyCruiser;
+      this.texture = texture.cruiser;
+      this.damageTexture = texture.cruiserDamage;
+      this.down = true;
+      this.verticalMov = 40;
+    }else if(type === shiptype.frigate){
+      this.w = 30;
+      this.h = 42;
+      this.maxHP = 2; // <- 2 shots
+      this.hp = this.maxHP-1;
+      this.speedX = 3;
+      this.texture = texture.frigate;
+      this.damageTexture = texture.frigateDamage;
+      this.evacuate = false;
+    }else if(type === shiptype.corvette){
+      this.w = 20;
+      this.h = 16;
+      this.speedX = 5;
+      this.texture = texture.corvette;
     }
     this.x = Width-10;
     this.enemy = true;
     this.fireTime = 0;
     this.y = Math.floor(Math.random() * (Height - (this.h*2) + 1));
+    this.originY = this.y+this.verticalMov;
   }
   projectile(){
     let time = Date.now();
@@ -193,7 +227,7 @@ KeyListener.prototype.addKeyPressListener = function (keyCode, callback) {
     if (e.keyCode == keyCode) callback(e);
   });
 };
-var keys = new KeyListener();
+let keys = new KeyListener();
 
 function update(){
   // canvas base
@@ -205,15 +239,15 @@ function update(){
   player.animate();
   player.drawTexture();
   // enemies spawn
-  let time = Date.now(),
-  spawnRate = Math.floor((Math.random() * (5000-500+1)))+ 500;
+  let time = Date.now(), 
+  spawnRate = Math.floor(Math.random() * (1500-250+1))+ 250;
   // see if its time to spawn a new object
   if (time > (lastSpawn + spawnRate)) {
       lastSpawn = time;
       enemiesArray.push(new Enemy(enemyType()));
   }
   // enemy move, collision with player, and garbage collection
-  for(var i = enemiesArray.length - 1; i >= 0; i--){
+  for(let i = enemiesArray.length - 1; i >= 0; i--){
     enemiesArray[i].move();
     if(enemiesArray[i].checkCollision(player)) gameOVER();
     enemiesArray[i].projectile();
@@ -221,13 +255,14 @@ function update(){
     if(enemiesArray[i].suicide()) enemiesArray.splice(i,1);
   }
   // projectile movement, collision(player, enemy), and garbage collection
-  for(var i = projectilesArray.length - 1; i >= 0; i--){
+  for(let i = projectilesArray.length - 1; i >= 0; i--){
     projectilesArray[i].move();
     if(projectilesArray[i].enemy && projectilesArray[i].checkCollision(player)) gameOVER();
     else if(!projectilesArray[i].enemy){
-      for(var k = enemiesArray.length - 1; k >= 0; k--){
+      for(let k = enemiesArray.length - 1; k >= 0; k--){
         if(projectilesArray[i].checkCollision(enemiesArray[k])){
-          enemiesArray.splice(k,1);
+          if(enemiesArray[k].hp) enemiesArray[k].hp--;
+          else enemiesArray.splice(k,1);          
           projectilesArray.splice(i,1);
           return;
         }
@@ -243,10 +278,9 @@ function gameOVER(){
   console.log("Game Over");
 }
 
-function loop() {
+(function loop() {
   if(gameOver)
     return;
   update();
   requestAnimationFrame(loop);
-}
-requestAnimationFrame(loop);
+})();
